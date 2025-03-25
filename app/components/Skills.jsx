e2a2221f5686d08873706fc19e130989703adb03
Najmus Sakib Rashid <sakib.buetian@gmail.com>
2025-03-25 13:04:25 +0600

    quick commit

---
+++ app/components/Skills.jsx
@@ -0,0 +1,32 @@
+import SkillFormat from "@/public/assets/scripts/skill_script.json";
+
+const Skills = () => {
+    return (
+        <>
+            <div className="mt-16 flex justify-start md:w-1/2 text-2xl font-semibold text-gray-800">
+                My Skills
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-[auto_auto] md:w-1/2 gap-x-16">
+                {SkillFormat.map((skill, index) => (
+                    <>
+                        <div className="text-sm font-medium text-gray-700 mt-2">
+                            {skill.title}
+                        </div>
+                        <div className="flex flex-wrap gap-2 mt-2">
+                            {skill.skills.map((skill, index) => (
+                                <div
+                                    key={index}
+                                    className="text-gray-700 text-sm bg-gray-200 px-2 py-1 rounded-md"
+                                >
+                                    {skill}
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default Skills;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Profile from "@/app/components/Profile.jsx";
 import Projects from "@/app/components/Projects.jsx";
+import Skills from "@/app/components/Skills.jsx";
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center">
             <Profile />
             <Projects />
+            <Skills />
         </div>
     );
 }
