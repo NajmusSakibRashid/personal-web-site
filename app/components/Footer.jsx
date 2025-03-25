5063b70f66fca4cd7f9ea915ac8ee86f110b5004
Najmus Sakib Rashid <sakib.buetian@gmail.com>
2025-03-25 12:29:55 +0600

    quick commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,24 +3,30 @@ const Footer = () => {
         <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
             <nav>
                 <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <a className="link link-hover">Web Development</a>
+                <a className="link link-hover">Database Design</a>
+                <a className="link link-hover">Software Engineering</a>
+                <a className="link link-hover">Machine Learning Engineer</a>
             </nav>
             <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <h6 className="footer-title">Contact</h6>
+                <a className="link link-hover">Phone: 01706966900</a>
+                <a className="link link-hover">
+                    Email: sakib.buetian@gmail.com
+                </a>
+                <a className="link link-hover">
+                    GitHub: https://github.com/NajmusSakibRashid
+                </a>
+                <a className="link link-hover">
+                    LinkedIn: https://www.linkedin.com/in/najmus-sakib-rashid/
+                </a>
             </nav>
-            <nav>
+            {/* <nav>
                 <h6 className="footer-title">Legal</h6>
                 <a className="link link-hover">Terms of use</a>
                 <a className="link link-hover">Privacy policy</a>
                 <a className="link link-hover">Cookie policy</a>
-            </nav>
+            </nav> */}
         </footer>
     );
 };
